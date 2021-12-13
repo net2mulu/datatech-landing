c4e3a27c5a719a94df826fe08609049bd2ccc7c1
net <nati2mulugeta@gmail.com>
2021-12-13 05:56:52 -0500

    tailwind and some comps created

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello About!</h1>
+      <p></p>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEventsPage() {
+    return (
+      <div>
+        <h1 className="font-bold text-red-500 mx-4">Add Event</h1>
+      </div>
+    );
+}
